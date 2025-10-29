89e766a03eba971c82538f76d141aadf13285f9b
Alvi-prog <alwinjoseph1100@gmail.com>
2025-10-30 01:31:40 +0530

    1:31am 30 oct 2025

--- mainspace.c
+++ mainspace.c
@@ -1,40 +1,82 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 FILE *fp;
-//void BUY();
-//void SELL();
-// void ADD();
-struct bike{
-  char brand[30],name[40];
+ 
+// void BUY();
+void SELL();
+void ADD();
+struct bike
+{
+  char brand[30], name[40];
   int egsize;
   char fueltype[10];
   float price;
-};
+} bikearray[1000000];
 
+struct customer
+{
+  char name[30];
+  int orderno;
+} custoarray[1000000];
+int c1;
 void main(void)
-{ do{
-  int c1;
-  printf("-----WELCOME TO BIKE DEALERSHIP SOFTWARE-----\n");
-  printf("   what would you like to do? \n Press the number for the following \n 1.BUY bike \n 2.SELL bike \n 3.ADD bike \n 4.EXIT soft \n");
-  scanf("%d",&c1);
-if(c1==1){
- //BUY();
-}
-else if(c1==2){
- //SELL();
-}
-else if(c1==3){
- ADD();
-}else if(c1=4){
-  printf("EXITING.....");
-  exit(0);
+{
+  do
+  {
+
+    printf("-----WELCOME TO BIKE DEALERSHIP SOFTWARE-----\n");
+    printf("   what would you like to do? \n Press the number for the following \n 1.BUY bike \n 2.SELL bike \n 3.ADD bike \n 4.EXIT soft \n");
+    scanf("%d", &c1);
+    if (c1 == 1)
+    {
+      // BUY();
+    }
+  else if (c1 == 2)
+    {
+      SELL();
+    }
+    else if (c1 == 3)
+    {
+      ADD();
+    }
+    else if (c1 == 4)
+    {
+      printf("EXITING.....");
+      exit(0);
+    }
+
+  } while (c1 < 5 && c1 > 0);
 }
 
-}while(1);
+void SELL()
+{ //fp=fopen("database","a");
+  int sellno=0;
+  printf("ENTER YOUR NAME: ");
+   scanf(" %[^\n]",custoarray[sellno].name);
+   custoarray[sellno].orderno=sellno;
+ 
 }
 
-void ADD(){
-printf("GIVE EMPLOYEE NAME \n");
-}
+void ADD()
+{
+  char empname[30];
+  printf("GIVE EMPLOYEE NAME: ");
+  scanf("%s", &empname);
 
-//testtttt
+  int pass = 0, times = 0;
+  do
+  {
+    printf("GIVE PASSWORD: ");
+    scanf("%d", &pass);
+    if (pass == 123)
+    {
+      printf("test ok");
+      exit(0);
+    }
+    else
+    {
+      ++times;
+      printf("INVALID PASSWORD! try again\n");
+    }
+  } while (times < 4 || pass != 123);
+}
